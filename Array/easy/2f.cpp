0550296b0b1abdd424aea9434edfc06aa16d5f01
Dhritideepa Gorai <dhriti@Dhritis-MacBook-Air.local>
2023-02-18 09:59:59 +0530

    chocolate problem

---
+++ Array/easy/2f.cpp
@@ -0,0 +1,52 @@
+//Chocolate Distribution Problem
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    
+    sort(a.begin(),a.end());
+    
+    int min = a[m-1] - a[0];
+    int diff;
+    
+    for(int i=0,j=i+m-1; j<a.size(); i++,j++){
+        diff = (a[j] - a[i]);
+        if( diff < min)
+            min = diff;
+    }
+    
+    return min;
+    
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
